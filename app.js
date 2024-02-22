b29d173014971a17fc2c506ddeb2751395bee52b
Shaafkashan <shaafkashan245@gmail.com>
2024-02-22 16:16:16 +0000

    Update app.js
    handle error to prevent hanging or crashing by putting it into a try catch block

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
   const stockSymbols = await stocks.getStocks()
+  console.log(stockSymbols);
   res.send({ stockSymbols })
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+  try {
+    const { params: { symbol } } = req
+    const data = await stocks.getStockPoints(symbol, new Date().getTime());
+    res.send(data);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 
 app.listen(3000, () => console.log('Server is running!'))
